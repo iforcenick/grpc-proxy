a12bd7d20927f0006e9509a557b3532e52594dce
iforcenick <felixsoft.dev@gmail.com>
2022-09-19 04:04:30 -0400

    feat: basic requirement accomplished

--- client/client.go
+++ client/client.go
@@ -2,21 +2,71 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"time"
 	tmclient "tmclient"
+
+	"io/ioutil"
 
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 )
 
-func main() {
-	client := tmclient.Dial("localhost:8080")
-	response, err := (*client).GetLatestBlock(context.Background(), &tmservice.GetLatestBlockRequest{})
-	if err != nil {
-		log.Fatal("Unable to get latest block")
-		return
-	}
-	block_hash := response.BlockId.Hash
-	block_height := response.Block.Header.Height
+type BlockInfo struct {
+	height int64
+	hash   []byte
+}
 
-	log.Print(block_hash, block_height)
+func main() {
+	ticker := time.NewTicker(1000 * time.Millisecond)
+	fetch_chan := make(chan BlockInfo)
+	block_queue := []BlockInfo{}
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				client := tmclient.Dial("localhost:8080")
+				response, err := (*client).GetLatestBlock(context.Background(), &tmservice.GetLatestBlockRequest{})
+				if err != nil {
+					log.Fatal("Unable to get latest block")
+					return
+				}
+				block_hash := response.BlockId.Hash
+				block_height := response.Block.Header.Height
+				fetch_chan <- BlockInfo{hash: block_hash, height: block_height}
+			}
+		}
+	}()
+
+	previous_block_height := 0
+	for true {
+		block := <-fetch_chan
+		if block.height == int64(previous_block_height) {
+			continue
+		}
+
+		previous_block_height = int(block.height)
+		block_queue = append(block_queue, block)
+		if len(block_queue) > 5 {
+			block_queue = block_queue[1:]
+		}
+
+		block_json := make([]map[string]interface{}, len(block_queue))
+
+		for i := range block_queue {
+			block_json[i] = map[string]interface{}{
+				"height": block_queue[i].height,
+				"hash":   block_queue[i].hash,
+			}
+		}
+		buffer, err := json.MarshalIndent(map[string]interface{}{
+			"test_result": block_json,
+		}, "", " ")
+		if err != nil {
+			log.Fatalf("Unable to marshal json data: %v", err)
+		}
+		_ = ioutil.WriteFile("block_status.json", buffer, 0644)
+		log.Print(buffer)
+	}
 }
--- server/grpc_test.go
+++ server/grpc_test.go
@@ -1,9 +1,82 @@
 package main
 
 import (
+	"context"
+	"reflect"
 	"testing"
+	tmclient "tmclient"
+
+	tmservice "github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 )
 
-func TestEquality(t *testing.T) {
+func TextGetLatestBlock(t *testing.T) {
+	local_client := tmclient.Dial("localhost:8080")
+	local_response, local_err := (*local_client).GetLatestBlock(context.Background(), &tmservice.GetLatestBlockRequest{})
+	if local_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
 
+	remote_client := tmclient.Dial("grpc.osmosis.zone:9090")
+	remote_response, remote_err := (*remote_client).GetLatestBlock(context.Background(), &tmservice.GetLatestBlockRequest{})
+	if remote_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	if !reflect.DeepEqual(local_response, remote_response) {
+		t.Errorf("Expected %v, got %v", remote_response, local_response)
+	}
+}
+
+func TestGetBlock(t *testing.T) {
+	local_client := tmclient.Dial("localhost:8080")
+	local_response, local_err := (*local_client).GetBlockByHeight(context.Background(), &tmservice.GetBlockByHeightRequest{Height: 5850706})
+	if local_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	remote_client := tmclient.Dial("grpc.osmosis.zone:9090")
+	remote_response, remote_err := (*remote_client).GetBlockByHeight(context.Background(), &tmservice.GetBlockByHeightRequest{Height: 5850706})
+	if remote_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	if !reflect.DeepEqual(local_response, remote_response) {
+		t.Errorf("Expected %v, got %v", remote_response, local_response)
+	}
+}
+
+func TestGetSyncing(t *testing.T) {
+	local_client := tmclient.Dial("localhost:8080")
+	local_response, local_err := (*local_client).GetSyncing(context.Background(), &tmservice.GetSyncingRequest{})
+	if local_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	remote_client := tmclient.Dial("grpc.osmosis.zone:9090")
+	remote_response, remote_err := (*remote_client).GetSyncing(context.Background(), &tmservice.GetSyncingRequest{})
+	if remote_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	if !reflect.DeepEqual(local_response, remote_response) {
+		t.Errorf("Expected %v, got %v", remote_response, local_response)
+	}
+}
+
+func TestGetNodeInfo(t *testing.T) {
+	local_client := tmclient.Dial("localhost:8080")
+	local_response, local_err := (*local_client).GetNodeInfo(context.Background(), &tmservice.GetNodeInfoRequest{})
+	if local_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	remote_client := tmclient.Dial("grpc.osmosis.zone:9090")
+	remote_response, remote_err := (*remote_client).GetNodeInfo(context.Background(), &tmservice.GetNodeInfoRequest{})
+	if remote_err != nil {
+		t.Errorf("Error occured with %v", local_err)
+	}
+
+	if !reflect.DeepEqual(local_response, remote_response) {
+		t.Errorf("Expected %v, got %v", remote_response, local_response)
+	}
 }
