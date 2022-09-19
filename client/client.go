8acb24a868e9e5d0a10cba4b27bb7dc7b16b9d97
iforcenick <felixsoft.dev@gmail.com>
2022-09-18 22:15:07 -0400

    feat: client initialized

--- client/client.go
+++ client/client.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	tmclient "tmclient"
 
+	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
+)
+
+func main() {
+	client := tmclient.Dial("localhost:8080")
+	response, err := (*client).GetLatestBlock(context.Background(), &tmservice.GetLatestBlockRequest{})
+	if err != nil {
+		log.Fatal("Unable to get latest block")
+		return
+	}
+	block_hash := response.BlockId.Hash
+	block_height := response.Block.Header.Height
+
+	log.Print(block_hash, block_height)
 }
--- client/go.mod
+++ client/go.mod
@@ -1,3 +1,11 @@
 module client
 
 go 1.19
+
+require (
+    tmclient v0.0.0
+)
+
+replace (
+    tmclient v0.0.0 => ./../tmclient/
+)
